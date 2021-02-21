16fb7c503598bf88193606a18e14f002b90fe5e4
Danny Berger <danberadi@gmail.com>
2021-02-21 15:04:15 -0500

    Format Contact Form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,28 +11,28 @@ const Contact = () => {
   return (
     <section id="contact">
       <Container>
-        <Title title="Get In Touch!" />
+        <Title title="Contact" />
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
-            {/* <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with me? Awesome!'}
-            </p> */}
             <form name="contact" method="POST" data-netlify="true" attribute="netlify-honeypot='bot-field'">
               <input type="hidden" name="bot-field" />
               <input type="hidden" name="form-name" value="contact" />
-              <label>Your Name: <input type="text" name="name" /></label>
-              <label>Your Email: <input type="email" name="email" /></label>
-              <label>Message: <textarea name="message"></textarea></label>
-              <button type="submit">Send</button>
+              <label className="form-label">
+                Name:
+                <input className="form-input" type="text" name="name" />
+              </label>
+              <label className="form-label">
+                Email:
+                <input type="email" name="email" />
+              </label>
+              <label className="form-label">
+                Message:
+                <textarea name="message"></textarea>
+              </label>
+              <button className="cta-btn cta-btn--hero" type="submit">
+                Send
+              </button>
             </form>
-            {/* <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
-            >
-              {btn || "Let's Talk"}
-            </a> */}
           </div>
         </Fade>
       </Container>
